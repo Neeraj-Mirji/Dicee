c8c13ac9ca83bb36da092621056ecba8277dbc1a
Neeraj-Mirji <61899469+Neeraj-Mirji@users.noreply.github.com>
2021-03-15 20:14:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+if (performance.navigation.type == 1)
+{
+var player1 = Math.floor(Math.random()*6) + 1;
+var player2 = Math.floor(Math.random()*6) + 1;
+
+
+
+document.querySelector(".img1").setAttribute("src" , "images/dice" + player1 + ".png");
+document.querySelector(".img2").setAttribute("src" , "images/dice" + player2 + ".png");
+
+if(player1 > player2 ) document.querySelector("h1").textContent = "🚩 player1 wins !";
+else if (player1 < player2) document.querySelector("h1").textContent = "player2 wins ! 🚩";
+     else document.querySelector("h1").textContent = "Draw";
+}
